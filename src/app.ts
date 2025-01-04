55bde766724cca603db670e42001ad394f1e4a68
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-01-03 19:16:57 -0800

    Create estimates api

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import userRoutes from './routes/userRoutes';
 import projectRoutes from './routes/projectRoutes';
 import taskRoutes from './routes/taskRoutes';
 import budgetRoutes from './routes/budgetRoutes';
+import estimateRoutes from './routes/estimateRoutes';
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/budgets', budgetRoutes);
+app.use('/api/estimates', estimateRoutes);
 
 export default app;
