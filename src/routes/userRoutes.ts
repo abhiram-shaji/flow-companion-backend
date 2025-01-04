6fe521126703d2ad1c76bafcf0be5285c4c614ad
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-01-03 17:26:40 -0800

    Scaffold project

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,1 +1,10 @@
-  
+import express from 'express';
+import { addUser, getAllWorkers } from '../controllers/userController';
+import { simpleAuth } from '../middleware/authMiddleware';
+
+const router = express.Router();
+
+router.post('/', simpleAuth, addUser);
+router.get('/workers', simpleAuth, getAllWorkers);
+
+export default router;
