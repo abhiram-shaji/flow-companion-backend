8c45a3ebd80ba29fcdf6bc4f64b42498c6c96199
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-01-14 19:06:37 -0800

    Update budget and project routes

--- src/routes/budgetRoutes.ts
+++ src/routes/budgetRoutes.ts
@@ -7,7 +7,7 @@ router.get('/project/:projectId', getBudgetByProject); // Get Budget by Project
 router.put('/:id', updateBudget); // Update Budget
 
 // Route to fetch all budgets
-router.get('/all-budgets', getAllBudgets);
+router.get('/get-all', getAllBudgets);
 
 // Route to fetch budgets for a specific user (using request body)
 router.post('/assigned-to', getBudgetsByAssignedUser);
