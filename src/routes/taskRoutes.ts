91832ca8428d55d1cf4e6528a654c4af4c63f2b4
Abhiram <write4abhiram@gmail.com>
2025-01-14 16:16:20 -0800

    Update task routes

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -4,8 +4,8 @@ import { addTask, getTasksByProject, editTask, deleteTask, getTasksByAssignedTo,
 const router = express.Router();
 
 router.post('/', addTask); // Add Task
-router.post('/api/tasks/assigned-to', getTasksByAssignedTo);
-router.get('/api/tasks', getAllTasks);
+router.post('/assigned-to', getTasksByAssignedTo);
+router.get('/get-all', getAllTasks);
 router.get('/project/:projectId', getTasksByProject); // Get Tasks by Project
 router.put('/:id', editTask); // Edit Task
 router.delete('/:id', deleteTask); // Delete Task
